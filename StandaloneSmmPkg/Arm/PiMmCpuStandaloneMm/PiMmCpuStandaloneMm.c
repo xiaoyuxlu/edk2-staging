44f309dfe60da932353a4c9be1bfc86dd88b467c
Achin Gupta <achin.gupta@arm.com>
2016-07-18 12:14:18 +0100

    StandaloneSmmPkg: Get NS communication buffer details from Hoblist
    On ARMv8-A platforms, synchronous communication between the MM
    environment in the secure world and the UEFI runtime environment or
    the OS in the normal world will take place through the MM_COMMUNICATE
    SMC. A parameter of this SMC is a communication buffer. It is possible
    for the caller to specify a different buffer for every call that would
    have to subsequently mapped into the secure world.
    
    However, it is more optimal if the secure world pre-allocated a buffer
    and provided it extents to both the normal world and the MM
    environment. This approach replaces the overhead of mapping a buffer
    into the secure world translation tables potentially for every
    MM_COMMUNICATE SMC with a simple bounds check of the buffer.
    
    The software entity responsible for initialising the MM Foundation
    provides the details of this buffer in one of the
    EFI_SMRAM_DESCRIPTORs in the EFI_SMRAM_HOB_DESCRIPTOR_BLOCK. The
    EFI_SMRAM_STATE for this SMRAM range is marked with a non-standard
    attribute. This patch adds support for retrieving the extents of this
    buffer from the Hoblist and making a private copy of this information.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Achin Gupta <achin.gupta@arm.com>

--- StandaloneSmmPkg/Arm/PiMmCpuStandaloneMm/PiMmCpuStandaloneMm.c
+++ StandaloneSmmPkg/Arm/PiMmCpuStandaloneMm/PiMmCpuStandaloneMm.c
@@ -20,17 +20,24 @@
 
 #include <Library/DebugLib.h>
 #include <Library/BaseMemoryLib.h>
+#include <Library/HobLib.h>
 
 #include <Protocol/MmConfiguration.h>
 #include <Protocol/SmmCpu.h>
 #include <Protocol/DebugSupport.h> // for EFI_SYSTEM_CONTEXT
 
+#include <Guid/SmramMemoryReserve.h>
 EFI_STATUS
 EFIAPI
 ArmRegisterMmFoundationEntry (
   IN CONST EFI_MM_CONFIGURATION_PROTOCOL  *This,
   IN EFI_SMM_ENTRY_POINT                  MmEntryPoint
   );
+
+// TODO: Non standard flag defined by ARM TF to mark the SMRAM descriptor that
+// contains the extents of the buffer to be used for communication with the
+// normal world
+#define EFI_SECURE_NS_MEM             0x00000080
 
 //
 // Private copy of the MM system table for future use
@@ -42,6 +49,9 @@ EFI_SMM_SYSTEM_TABLE2 *mSmst = NULL;
 //
 EFI_HANDLE            mMmCpuHandle = NULL;
 
+// Descriptor with whereabouts of memory used for communication with the normal world
+EFI_SMRAM_DESCRIPTOR  mNsCommBuffer;
+
 EFI_MM_CONFIGURATION_PROTOCOL mMmConfig = {
   ArmRegisterMmFoundationEntry
 };
@@ -49,12 +59,40 @@ EFI_MM_CONFIGURATION_PROTOCOL mMmConfig = {
 EFI_SMM_ENTRY_POINT     mMmEntryPoint = NULL;
 
 EFI_STATUS
+GetGuidedHobData (
+  IN  VOID *HobList,
+  IN  CONST EFI_GUID *HobGuid,
+  OUT VOID **HobData)
+{
+  EFI_HOB_GUID_TYPE *Hob;
+
+  if (!HobList || !HobGuid || !HobData)
+    return EFI_INVALID_PARAMETER;
+
+  Hob = GetNextGuidHob (HobGuid, HobList);
+  if (!Hob)
+    return EFI_NOT_FOUND;
+
+  *HobData = GET_GUID_HOB_DATA (Hob);
+  if (!HobData)
+    return EFI_NOT_FOUND;
+
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS
 PiMmCpuStandaloneMmInitialize (
   IN EFI_HANDLE         ImageHandle,  // not actual imagehandle
   IN EFI_SMM_SYSTEM_TABLE2   *SystemTable  // not actual systemtable
   )
 {
+  EFI_SMRAM_HOB_DESCRIPTOR_BLOCK  *SmramRangesHobData;
+  EFI_CONFIGURATION_TABLE         *ConfigurationTable;
+  EFI_SMRAM_DESCRIPTOR            *SmramRanges;
   EFI_STATUS                       Status;
+  UINT32                           SmramRangeCount;
+  UINTN                            Index;
+  VOID                            *HobStart;
 
   ASSERT (SystemTable != NULL);
   mSmst = SystemTable;
@@ -65,6 +103,67 @@ PiMmCpuStandaloneMmInitialize (
   if (EFI_ERROR(Status)) {
     return Status;
   }
+
+  // Retrieve the Hoblist from the SMST to extract the details of the NS
+  // communication buffer that has been reserved by S-EL1/EL3
+  ConfigurationTable = mSmst->SmmConfigurationTable;
+  for (Index = 0; Index < mSmst->NumberOfTableEntries; Index++) {
+    if (CompareGuid (&gEfiHobListGuid, &(ConfigurationTable[Index].VendorGuid))) {
+      break;
+    }
+  }
+
+  // Bail out if the Hoblist could not be found
+  // TODO: This could also mean that
+  // the normal world will never interact synchronously with the MM environment
+  if (Index >= mSmst->NumberOfTableEntries) {
+    DEBUG ((EFI_D_INFO, "Hoblist not found - 0x%x\n", Index));
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  HobStart = ConfigurationTable[Index].VendorTable;
+
+  //
+  // Extract the SMRAM ranges from the SMRAM descriptor HOB
+  //
+  Status = GetGuidedHobData (HobStart,
+			     &gEfiSmmPeiSmramMemoryReserveGuid,
+			     (VOID **) &SmramRangesHobData);
+  if (EFI_ERROR(Status)) {
+    DEBUG ((EFI_D_INFO, "SmramRangesHobData extraction failed - 0x%x\n", Status));
+    return Status;
+  }
+
+  SmramRanges = SmramRangesHobData->Descriptor;
+  SmramRangeCount = SmramRangesHobData->NumberOfSmmReservedRegions;
+  ASSERT (SmramRanges);
+  ASSERT (SmramRangeCount);
+
+  DEBUG ((EFI_D_INFO, "SmramRangeCount - 0x%x\n", SmramRangeCount));
+  for (Index = 0; Index < SmramRangeCount; Index++) {
+    DEBUG ((EFI_D_INFO, "SmramRanges[%d]: 0x%016lx - 0x%lx - 0x%lx\n", Index,
+	    SmramRanges[Index].CpuStart,
+	    SmramRanges[Index].PhysicalSize,
+	    SmramRanges[Index].RegionState));
+  }
+
+  // Find the descriptor that contains the whereabouts of the buffer for
+  // communication with the Normal world.
+  for (Index = 0; Index < SmramRangeCount; Index++) {
+     if (SmramRanges[Index].RegionState & EFI_SECURE_NS_MEM) {
+	     break;
+     }
+  }
+
+  DEBUG ((EFI_D_INFO, "mNsCommBuffer SmramRangeindex %d\n", Index));
+
+  if (Index >= SmramRangeCount)
+    return EFI_OUT_OF_RESOURCES;
+
+  CopyMem(&mNsCommBuffer, &SmramRanges[Index], sizeof(EFI_SMRAM_DESCRIPTOR));
+  DEBUG ((EFI_D_INFO, "mNsCommBuffer: 0x%016lx - 0x%lx\n",
+	  mNsCommBuffer.CpuStart,
+	  mNsCommBuffer.PhysicalSize));
 
   return Status;
 }
