4ef294a4d9bc79777299dc6e82c90dfa44a7a3d5
Xiaoyu Lu <xiaoyux.lu@intel.com>
2019-07-09 19:00:42 +0800

    MdePkg: Copy host based unit tests from HBFA
    Copy tests from HBFA/UefiHostUnitTestCasePkg/TestCase/MdePkg/Library
    
    1. DxeServicesTable -> MdePkg/Library/DxeServicesTableLib/Tests
    2. PeiServicesLib -> MdePkg/Library/PeiServicesLib/Tests
    3. SmmServicesTableLib -> MdePkg/Library/SmmServicesTableLib/Tests
    4. UefiBootServicesTableLib -> MdePkg/Library/UefiBootServicesTableLib/Tests
    5. MemoryAllocationLib -> MdePkg/Library/UefiMemoryAllocationLib/Tests
    6. UefiRuntimeServicesTableLib -> MdePkg/Library/UefiRuntimeServicesTableLib/Tests
    
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Liming Gao <liming.gao@intel.com>
    Signed-off-by: Xiaoyu Lu <xiaoyux.lu@intel.com>

---
+++ MdePkg/Library/PeiServicesLib/Tests/TestPeiServicesLib.c
@@ -0,0 +1,90 @@
+/** @file
+
+  Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <PiPei.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/PeiServicesLib.h>
+
+#include <UnitTestTypes.h>
+#include <Library/UnitTestLib.h>
+#include <Library/UnitTestAssertLib.h>
+
+#define UNIT_TEST_NAME        L"PeiServices Unit Test"
+#define UNIT_TEST_VERSION     L"0.1"
+
+#define MAX_STRING_SIZE  1025
+
+EFI_GUID gTestPpiGuid = {0x21a474cf, 0x7044, 0x4199, {0xa3, 0x4d, 0x82, 0xc4, 0xf1, 0xc, 0x9e, 0xc3}};
+
+UNIT_TEST_STATUS
+EFIAPI
+TestPpiDatabase (
+  IN UNIT_TEST_FRAMEWORK_HANDLE  Framework,
+  IN UNIT_TEST_CONTEXT           Context
+  )
+{
+  EFI_STATUS  Status;
+  EFI_PEI_PPI_DESCRIPTOR   PpiList = {
+    (EFI_PEI_PPI_DESCRIPTOR_PPI | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
+    &gTestPpiGuid,
+    NULL
+  };
+
+  Status = PeiServicesInstallPpi (
+             &PpiList
+             );
+  UT_ASSERT_NOT_EFI_ERROR(Status);
+  return UNIT_TEST_PASSED;
+}
+
+/**
+  The main() function for setting up and running the tests.
+
+  @retval EFI_SUCCESS on successful running.
+  @retval Other error code on failure.
+**/
+int main()
+{
+  EFI_STATUS                Status;
+  UNIT_TEST_FRAMEWORK       *Fw;
+  UNIT_TEST_SUITE           *TestSuite;
+  CHAR16                    ShortName[MAX_STRING_SIZE];
+
+  Fw = NULL;
+  TestSuite = NULL;
+
+  AsciiStrToUnicodeStrS (gEfiCallerBaseName, ShortName, sizeof(ShortName)/sizeof(ShortName[0]));
+  DEBUG((DEBUG_INFO, "%s v%s\n", UNIT_TEST_NAME, UNIT_TEST_VERSION));
+
+  Status = InitUnitTestFramework (&Fw, UNIT_TEST_NAME, ShortName, UNIT_TEST_VERSION);
+  if (EFI_ERROR(Status)) {
+    DEBUG((DEBUG_ERROR, "Failed in InitUnitTestFramework. Status = %r\n", Status));
+    goto EXIT;
+  }
+
+  Status = CreateUnitTestSuite (&TestSuite, Fw, L"PeiServices Basic Test Suite", L"Common.PeiServices.Basic", NULL, NULL);
+  if (EFI_ERROR(Status)) {
+    DEBUG((DEBUG_ERROR, "Failed in CreateUnitTestSuite for PeiServices Basic Test Suite\n"));
+    Status = EFI_OUT_OF_RESOURCES;
+    goto EXIT;
+  }
+
+  AddTestCase(TestSuite, L"Test PpiDatabase", L"Common.PeiServices.Basic.PpiDatabase", TestPpiDatabase, NULL, NULL, NULL);
+
+  //
+  // Execute the tests.
+  //
+  Status = RunAllTestSuites(Fw);
+
+EXIT:
+  if (Fw != NULL) {
+    FreeUnitTestFramework(Fw);
+  }
+
+  return Status;
+}
---
+++ MdePkg/Library/SmmServicesTableLib/Tests/TestSmmServicesTableLib.c
@@ -0,0 +1,91 @@
+/** @file
+
+  Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+
+#include <PiSmm.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/SmmServicesTableLib.h>
+
+#include <UnitTestTypes.h>
+#include <Library/UnitTestLib.h>
+#include <Library/UnitTestAssertLib.h>
+
+#define UNIT_TEST_NAME        L"SmmServices Unit Test"
+#define UNIT_TEST_VERSION     L"0.1"
+
+#define MAX_STRING_SIZE  1025
+
+EFI_GUID gTestProtocolGuid = {0x21a474cf, 0x7044, 0x4199, {0xa3, 0x4d, 0x82, 0xc4, 0xf1, 0xc, 0x9e, 0xc3}};
+
+UNIT_TEST_STATUS
+EFIAPI
+TestProtocolDatabase (
+  IN UNIT_TEST_FRAMEWORK_HANDLE  Framework,
+  IN UNIT_TEST_CONTEXT           Context
+  )
+{
+  EFI_STATUS  Status;
+  EFI_HANDLE  Handle;
+
+  Handle = NULL;
+  Status = gSmst->SmmInstallProtocolInterface (
+                    &Handle,
+                    &gTestProtocolGuid,
+                    EFI_NATIVE_INTERFACE,
+                    NULL
+                    );
+  UT_ASSERT_NOT_EFI_ERROR(Status);
+  return UNIT_TEST_PASSED;
+}
+
+/**
+  The main() function for setting up and running the tests.
+
+  @retval EFI_SUCCESS on successful running.
+  @retval Other error code on failure.
+**/
+int main()
+{
+  EFI_STATUS                Status;
+  UNIT_TEST_FRAMEWORK       *Fw;
+  UNIT_TEST_SUITE           *TestSuite;
+  CHAR16                    ShortName[MAX_STRING_SIZE];
+
+  Fw = NULL;
+  TestSuite = NULL;
+
+  AsciiStrToUnicodeStrS (gEfiCallerBaseName, ShortName, sizeof(ShortName)/sizeof(ShortName[0]));
+  DEBUG((DEBUG_INFO, "%s v%s\n", UNIT_TEST_NAME, UNIT_TEST_VERSION));
+
+  Status = InitUnitTestFramework (&Fw, UNIT_TEST_NAME, ShortName, UNIT_TEST_VERSION);
+  if (EFI_ERROR(Status)) {
+    DEBUG((DEBUG_ERROR, "Failed in InitUnitTestFramework. Status = %r\n", Status));
+    goto EXIT;
+  }
+
+  Status = CreateUnitTestSuite (&TestSuite, Fw, L"SmmServices Basic Test Suite", L"Common.SmmServices.Basic", NULL, NULL);
+  if (EFI_ERROR(Status)) {
+    DEBUG((DEBUG_ERROR, "Failed in CreateUnitTestSuite for SmmServices Basic Test Suite\n"));
+    Status = EFI_OUT_OF_RESOURCES;
+    goto EXIT;
+  }
+
+  AddTestCase(TestSuite, L"Test ProtocolDatabase", L"Common.SmmServices.Basic.ProtocolDatabase", TestProtocolDatabase, NULL, NULL, NULL);
+
+  //
+  // Execute the tests.
+  //
+  Status = RunAllTestSuites(Fw);
+
+EXIT:
+  if (Fw != NULL) {
+    FreeUnitTestFramework(Fw);
+  }
+
+  return Status;
+}
